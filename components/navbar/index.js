ba2554cc616cf1777757c8c47fb9e7af4032463c
Godhani <godhani525@gmail.com>
2025-09-05 15:11:31 +0530

    update image logo

--- components/navbar/index.js
+++ components/navbar/index.js
@@ -4,8 +4,8 @@ function NavBar() {
     return (
         <div className='bg-primary1 w-full pt-1'>
             <a href="/">
-                <div className="h-[50px] flex  items-center justify-center ">
-                    <img src="/gameImage/logo.png" className=" h-[200px] py-5 " />
+                <div className="h-[60px] flex  items-center justify-center ">
+                    <img src="/gameImage/logo.png" className=" h-[100px] py-4" />
                 </div>
             </a>
         </div>
